f244d04a58bd1133d8546589309de5066d2585c2
Fliex <hubinde@live.com>
2026-06-25 10:38:24 +0800

    build(scripts): add proxy config to update pull and push commands
    为update.ps1的git操作添加代理配置，解决内网或受限网络下的拉取推送问题

--- update.ps1
+++ update.ps1
@@ -1,1 +1,2 @@
-git pull upstream master
+git -c http.proxy=http://127.0.0.1:7890 -c https.proxy=http://127.0.0.1:7890 pull upstream master
+git -c http.proxy=http://127.0.0.1:7890 -c https.proxy=http://127.0.0.1:7890 push origin master
