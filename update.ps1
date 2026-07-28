7a9d3721c160b3e1b25199b90d4ad3868535cdcc
Fliex <hubinde@live.com>
2026-07-28 15:04:22 +0800

    1

--- update.ps1
+++ update.ps1
@@ -1,2 +1,3 @@
 git -c http.proxy=http://127.0.0.1:7890 -c https.proxy=http://127.0.0.1:7890 pull upstream master
 git -c http.proxy=http://127.0.0.1:7890 -c https.proxy=http://127.0.0.1:7890 push origin master
+pause
